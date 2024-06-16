7d1d05641e219b1521ac8e03a8570f1c90edeacb
indokalpasaikia1 <117028126+indokalpasaikia1@users.noreply.github.com>
2024-06-16 18:27:40 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversionOperations.js
@@ -0,0 +1,34 @@
+// let score = "33abc"
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) // we get "NaN"-not a number but it's type is number.
+
+// score = undefined
+// valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) // we get "NaN"-not a number but its type is number.
+
+// score = null
+// valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) // we get 0, it is of type number.
+
+// score = true
+// valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) // we get 1,it is of type number.
+
+// let isLoggedIn = 0
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof(booleanIsLoggedIn))
+// console.log(booleanIsLoggedIn)// we get 'true' for all non-zero +ve integers.
+
+// ""-empty string => false
+// "indokalpa"-non-empty string => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof(stringNumber))
+console.log(stringNumber)
