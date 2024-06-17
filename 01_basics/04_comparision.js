291c96e0ff44baee73f0dd1a5b0d8a6b2dddb82f
indokalpasaikia1 <117028126+indokalpasaikia1@users.noreply.github.com>
2024-06-17 17:46:24 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); "2" converted to number. 
+// console.log("02" > 1);
+
+// console.log(null > 0); 'null' is conveted to 0
+// console.log(null >= 0); "null" is converted to 0
+// console.log(null == 0); equlaity and comparators work diffrently in JS.null is conveted to 'NaN'
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0)
+
+// '===' is strict ie same value and same data-type
+console.log("2" == 2);
+console.log("2" === 2)
