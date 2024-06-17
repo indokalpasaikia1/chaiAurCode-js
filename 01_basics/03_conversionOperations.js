e59847ded7b7fc8f267b41c0c88fed8daa24c10d
indokalpasaikia1 <117028126+indokalpasaikia1@users.noreply.github.com>
2024-06-17 16:43:51 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -28,7 +28,35 @@
 // ""-empty string => false
 // "indokalpa"-non-empty string => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof(stringNumber))
-console.log(stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof(stringNumber))
+// console.log(stringNumber)
+
+// ********** operations **********
+// let value = 3;
+// let negValue = -value;
+// //console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);// 2 raised to power 3
+// console.log(2/2);
+// console.log(2%3);// remainder
+
+// let str1 = "hello "
+// let str2 = "indokalpa"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);// we get 122 instead of 14 as 'string' first. 
+// console.log(1 + 2 + "2");// we get 32 here.
+
+console.log(+true);// converted to Number.
+console.log(+"");// converted to Number.
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.table([num1,num2,num3])
